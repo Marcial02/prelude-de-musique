cb14fb0c5d598c2dd83f1f1b7f54285b3335fa91
oriontech4ed-4 <oriontech4ed-4@oriontech4ed-4s-MacBook-Air.local>
2026-04-07 09:44:08 +0800

    Initial commit: Starting the Navbar and created custome theme.css (still empty)

---
+++ prelude-de-musique/src/Home.jsx
@@ -0,0 +1,10 @@
+import './theme.css'
+import Navbar from './Navbar.jsx'
+function Home (){
+
+   return (
+   <Navbar />
+  )
+
+}
+export default Home
---
+++ prelude-de-musique/src/Navbar.jsx
@@ -0,0 +1,26 @@
+import './theme.css'
+
+function Navbar (){
+
+    return(
+ <nav>
+
+    <span>
+        <a href="">
+        <img src="" alt="" />
+        </a>
+    </span>
+    
+    <span>
+    <a href="">Home</a>
+    <a href="">Teachers</a>
+    <a href=""></a>
+    </span>
+    
+
+</nav>
+
+    )
+   
+}
+export default Navbar
