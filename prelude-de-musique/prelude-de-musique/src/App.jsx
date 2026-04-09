00008a27c9ee9477b0988061233bebe97e8c72ad
Marzhiel Allan Cabusas <deltarunesusie@Marzhiels-MacBook-Pro.local>
2026-04-09 21:20:47 +0800

    fixed the duplicated folder

---
+++ prelude-de-musique/prelude-de-musique/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import Home from './Home.jsx'
+
+function App() {
+
+  return (
+   <Home />
+  )
+}
+
+export default App
---
+++ prelude-de-musique/prelude-de-musique/src/Navbar.jsx
@@ -0,0 +1,97 @@
+import { useState } from 'react'
+import './theme.css'
+import './mobile.css'
+import PDEMLogo from './assets/PDEM Logo.jpg'
+
+function Navbar (){
+const [activeMenu, setActiveMenu] = useState(null);
+const [isOpen, setIsOpen] = useState(false);
+const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+    return(
+ <nav className='navbar'>
+    <div>
+        {/*Brand Logo*/}
+        <a href="">
+        <img src={PDEMLogo} alt="" width={200} />
+        </a>
+    </div>
+    
+    <div className="hamburger" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+    </div>
+
+    <div className={`nav-menu ${isOpen ? "is-active" : ""}`}>
+ <ul className='nav-links'>
+        <li><a href="">About</a></li>
+        
+        <li 
+         onMouseEnter={() => setActiveMenu ('courses')}
+         onMouseLeave={() => setActiveMenu (null)}
+        >
+            <a href=""   > Courses Offered ▾</a>
+
+        {activeMenu === 'courses' && (
+            <ul className='dropdown-menu'>
+                <li><a href="">Piano Lesson</a></li>
+                <li><a href="">Voice Lesson</a></li>
+                <li><a href="">Viola Lesson</a></li>
+                <li><a href="">Guitar Lesson</a></li>
+                <li><a href="">Ukelele Lesson</a></li>
+                <li><a href="">Flute Lesson</a></li>
+                <li><a href="">Saxophone Lesson</a></li>
+                <li><a href="">Recorder Lesson</a></li>
+                <li><a href="">Drum Lesson</a></li>
+                <li><a href="">Trumphet Lesson</a></li>
+            </ul>
+            
+
+        )}
+        </li>
+
+        <li><a href="">Pricing</a></li>
+    </ul>
+
+     <div className='nav-action'>
+        <div
+        onMouseEnter={() => setActiveMenu ('enroll')}
+        onMouseLeave={() => setActiveMenu (null)}
+        >
+             <a href="">Enroll Now ▾</a>
+            
+        {activeMenu === 'enroll' && (
+            <ul className=' dropdown-menu'>
+                <li><a href="">Piano Branch</a></li>
+                <li><a href="">Voice Branch</a></li>
+                <li><a href="">Viola Branch</a></li>
+                <li><a href="">Guitar Branch</a></li>
+                <li><a href="">Ukelele Branch</a></li>
+                <li><a href="">Flute Branch</a></li>
+                <li><a href="">Saxophone Branch</a></li>
+                <li><a href="">Recorder Branch</a></li>
+                <li><a href="">Drum Branch</a></li>
+                <li><a href="">Trumphet Branch</a></li>
+                
+            </ul>
+        )}
+        </div>
+       
+        <a href="">Contact Us</a>
+    </div>
+    
+    </div>
+   
+
+    
+   
+    
+
+</nav>
+
+    )
+   
+}
+export default Navbar
