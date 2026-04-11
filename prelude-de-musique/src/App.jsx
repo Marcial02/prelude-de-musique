66d2ab193808fdf3a2e74df801c369fe55f0092e
Marzhiel Allan Cabusas <deltarunesusie@Marzhiels-MacBook-Pro.local>
2026-04-11 12:41:32 +0800

    Fixed Navbar and Hero. Imported Montserrat font

--- prelude-de-musique/src/App.jsx
+++ prelude-de-musique/src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import './css/global.css'
 import Home from './Home.jsx'
+import "@fontsource/montserrat"; // Defaults to weight 400
+import "@fontsource/montserrat/700.css"; // bold links/headers
 
 function App() {
 
