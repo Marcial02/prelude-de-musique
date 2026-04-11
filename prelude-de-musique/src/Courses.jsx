196d2f5db95151746100017bf53fcdf0f9788db7
Marzhiel Allan Cabusas <deltarunesusie@Marzhiels-MacBook-Pro.local>
2026-04-11 16:10:28 +0800

    Courses Offered

---
+++ prelude-de-musique/src/Courses.jsx
@@ -0,0 +1,73 @@
+import './css/courses.css'
+
+
+function Courses(){
+return(
+    <section>
+        <h1>COURSES OFFERED</h1>
+         <div className='course-container'>
+
+        <div className='course-item'>
+            <h3>Piano</h3>
+            <img src="./public/images/course1.jpg" alt="" />
+            <div className='course-item-overlay'></div>
+        </div>
+        <div className='course-item'>
+             <h3>Voice</h3>
+            <img src="./public/images/course2.jpg" alt="" />
+            <div className='course-item-overlay'></div>
+        </div>
+        <div className='course-item'>
+             <h3>Violin</h3>
+            <img src="./public/images/course3.jpg" alt="" />
+            <div className='course-item-overlay'></div>
+        </div>
+        <div className='course-item'>
+             <h3>Viola</h3>
+            <img src="./public/images/course4.jpg" alt="" />
+            <div className='course-item-overlay'></div>
+        </div>
+        <div className='course-item'>
+            <h3>Guitar</h3>
+            <img src="./public/images/course5.jpg" alt="" />
+            <div className='course-item-overlay'></div>
+        </div>
+        <div className='course-item'>
+            <h3>Ukelele</h3>
+            <img src="./public/images/course6.jpg" alt="" />
+            <div className='course-item-overlay'></div>
+        </div>
+        <div className='course-item'>
+            <h3>Flute</h3>
+            <img src="./public/images/course7.jpg" alt="" />
+            <div className='course-item-overlay'></div>
+        </div>
+        <div className='course-item'>
+            <h3>Recorder</h3>
+            <img src="./public/images/course8.jpg" alt="" />
+            <div className='course-item-overlay'></div>
+        </div>
+        <div className='course-item'>
+            <h3>Saxophone</h3>
+            <img src="./public/images/course9.jpg" alt="" />
+            <div className='course-item-overlay'></div>
+        </div>
+        <div className='course-item'>
+            <h3>Trumpet</h3>
+            <img src="./public/images/course10.jpg" alt="" />
+            <div className='course-item-overlay'></div>
+        </div>
+        <div className='course-item'>
+            <h3>Drum</h3>
+            <img src="./public/images/course11.jpg" alt="" />
+            <div className='course-item-overlay'></div>
+        </div>
+
+    </div>
+    </section>
+   
+)
+
+}
+
+export default Courses
--- prelude-de-musique/src/Home.jsx
+++ prelude-de-musique/src/Home.jsx
@@ -1,11 +1,13 @@
 import Navbar from './Navbar.jsx'
 import Hero from './Hero.jsx'
+import Courses from './Courses.jsx'
 function Home (){
 
    return (
     <>
    <Navbar />
    <Hero />
+   <Courses />
    </>
   )
 
