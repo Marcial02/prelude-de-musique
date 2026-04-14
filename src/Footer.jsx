f5bcfda90356346f21305fe7cf410ca8e9d48678
oriontech4ed-4 <oriontech4ed-4@oriontech4ed-4s-MacBook-Air.local>
2026-04-14 11:35:16 +0800

    starting footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,23 @@
+import './css/footer.css'
+import { Link } from 'react-router-dom'
+import { HashLink } from 'react-router-hash-link'
+
 function Footer(){
     return(
-        <section></section>
+        <section id="footer" className="footer-section">
+            <div className='footer-container'>
+                <ul>
+                    <li><HashLink to=''>Home</HashLink></li>
+                    <li><HashLink to=''>Courses Offered</HashLink></li>
+                    <li><Link to=''>Price</Link></li>
+                    <li><Link to=''>Enroll Now</Link></li>
+                    <li><HashLink to=''>Contact</HashLink></li>
+                </ul>
+                <a className='keonite' href="keonite.com">© Keonite.com</a>
+
+            </div>
+
+        </section>
     )
 }
 
