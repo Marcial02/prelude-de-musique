87d79c6e4071806f4c2aba948e89185f419ee831
Marzhiel Allan Cabusas <deltarunesusie@Marzhiels-MacBook-Pro.local>
2026-04-17 18:15:38 +0800

    saved images upto recitals 11

--- src/js/recitals.js
+++ src/js/recitals.js
@@ -1,4 +1,4 @@
-import { r8, r9 } from "./recitalsImage.js"
+import { r8, r9, r10, r11 } from "./recitalsImage.js"
 
 // Heto na! Isang loop lang, gawa na ang 19 objects mo.
 export const recitals8 = r8.map((img, index) => ({
@@ -9,6 +9,18 @@ export const recitals8 = r8.map((img, index) => ({
 
 export const recitals9 = r9.map((img, index) => ({
     id: index + 1,
-    image: img, // Ito yung kukuha sa r81, r82, etc.
+    image: img, 
+    name: ''
+}));
+
+export const recitals10 = r10.map((img, index) => ({
+    id: index + 1,
+    image: img, 
+    name: ''
+}));
+
+export const recitals11 = r11.map((img, index) => ({
+    id: index + 1,
+    image: img, 
     name: ''
 }));
--- src/js/recitalsImage.js
+++ src/js/recitalsImage.js
@@ -1,8 +1,13 @@
 // 1. Isang line lang, kukunin na lahat ng images sa loob ng folder
-const modules = import.meta.glob('/public/images/r8*.jpg', { eager: true });
-
+const modules1 = import.meta.glob('/public/images/r8*.jpg', { eager: true });
 // 2. I-loop natin yung nakuha nating "modules" para maging array
-export const r8 = Object.values(modules).map((mod) => mod.default);
+export const r8 = Object.values(modules1).map((mod) => mod.default);
 
 const modules2 = import.meta.glob('/public/images/r9*.jpg', { eager: true });
 export const r9 = Object.values(modules2).map((mod) => mod.default);
+
+const modules3 = import.meta.glob('/public/images/r10*.jpg', { eager: true });
+export const r10 = Object.values(modules3).map((mod) => mod.default);
+
+const modules4 = import.meta.glob('/public/images/r11*.jpg', { eager: true });
+export const r11 = Object.values(modules4).map((mod) => mod.default);
