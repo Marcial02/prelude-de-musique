70466be6dd2a2cdd85ad5c16804884d61ff50fb5
Marzhiel Allan Cabusas <deltarunesusie@Marzhiels-MacBook-Pro.local>
2026-04-18 09:52:27 +0800

    fixed spacing in lessons

--- src/Courses.jsx
+++ src/Courses.jsx
@@ -1,5 +1,7 @@
 import './css/courses.css'
 import { courseImage } from './js/images'
+import { HashLink } from 'react-router-hash-link';
+
 
 
 function Courses(){
@@ -9,96 +11,96 @@ return(
          <div className='course-container'>
 
         <div className='course-item'>
-            <a href="">
+            <HashLink to="/lessons#piano">
             <div className='course-item-text-container'>
                  <h3>Piano</h3>
             <p>Suitable for ages 4 and above</p>
             </div>
             <img src={courseImage.course1} alt="" />
-            <div className='course-item-overlay'></div></a>
+            <div className='course-item-overlay'></div></HashLink>
         </div>
         <div className='course-item'>
-            <a href="">
+            <HashLink to="/lessons#voice">
             <div className='course-item-text-container'>
              <h3>Voice</h3>
             <p>Suitable for ages 4 and above</p>
             </div>
             <img src={courseImage.course2} alt="" />
-            <div className='course-item-overlay'></div></a>
+            <div className='course-item-overlay'></div></HashLink>
         </div>
        
         <div className='course-item'>
-            <a href="">
+            <HashLink to="/lessons#violin">
             <div className='course-item-text-container'>
              <h3>Violin</h3>
              <p>Suitable for ages 5 and above</p>
              </div>
             <img src={courseImage.course3} alt="" />
-            <div className='course-item-overlay'></div></a>
+            <div className='course-item-overlay'></div></HashLink>
         </div>
         <div className='course-item'>
-            <a href="">
+            <HashLink to="/lessons#viola">
             <div className='course-item-text-container'>
              <h3>Viola</h3>
              <p>Suitable for ages 6 and above</p></div>
             <img src={courseImage.course4} alt="" />
-            <div className='course-item-overlay'></div></a>
+            <div className='course-item-overlay'></div></HashLink>
         </div>
         <div className='course-item'>
-            <a href="">
+            <HashLink to="/lessons#guitar">
             <div className='course-item-text-container'>
             <h3>Guitar</h3>
             <p>Suitable for ages 6 and above</p></div>
             <img src={courseImage.course5} alt="" />
-            <div className='course-item-overlay'></div></a>
+            <div className='course-item-overlay'></div></HashLink>
         </div>
         <div className='course-item'>
-            <a href="">
+            <HashLink to="/lessons#ukelele">
             <div className='course-item-text-container'>
             <h3>Ukelele</h3>
             <p>Suitable for ages 5 and above</p></div>
             <img src={courseImage.course6} alt="" />
-            <div className='course-item-overlay'></div></a>
+            <div className='course-item-overlay'></div></HashLink>
         </div>
         <div className='course-item'>
-            <a href="">
+            <HashLink to="/lessons#flute">
             <div className='course-item-text-container'>
             <h3>Flute</h3>
             <p>Suitable for ages 9 and above</p></div>
             <img src={courseImage.course7} alt="" />
-            <div className='course-item-overlay'></div></a>
+            <div className='course-item-overlay'></div></HashLink>
         </div>
         <div className='course-item'>
-            <a href="">
+            <HashLink to="/lessons#recorder">
             <div className='course-item-text-container'>
             <h3>Recorder</h3>
             <p>Suitable for ages 6 and above</p></div>
             <img src={courseImage.course8} alt="" />
-            <div className='course-item-overlay'></div></a>
+            <div className='course-item-overlay'></div></HashLink>
         </div>
         <div className='course-item'>
-            <a href="">
+            <HashLink to="/lessons#saxophone">
             <div className='course-item-text-container'>
             <h3>Saxophone</h3>
             <p>Suitable for ages 9 and above</p></div>
             <img src={courseImage.course9} alt="" />
-            <div className='course-item-overlay'></div></a>
+            <div className='course-item-overlay'></div></HashLink>
         </div>
         <div className='course-item'>
-            <a href="">
+            <HashLink to="/lessons#trumpet">
             <div className='course-item-text-container'>
             <h3>Trumpet</h3>
             <p>Suitable for ages 8 and above</p></div>
             <img src={courseImage.course10} alt="" />
-            <div className='course-item-overlay'></div></a>
+            <div className='course-item-overlay'></div></HashLink>
         </div>
         <div className='course-item'>
-            <a href="">
+            <HashLink to="/lessons#drum">
             <div className='course-item-text-container'>
             <h3>Drum</h3>
             <p>Suitable for ages 5 and above</p></div>
             <img src={courseImage.course11} alt="" />
-            <div className='course-item-overlay'></div></a>
+            <div className='course-item-overlay'></div></HashLink>
         </div>
 
     </div>
